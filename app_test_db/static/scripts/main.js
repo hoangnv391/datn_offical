4d2d2c853ae5f73f2e6eb4249c523846204c2116
hoangnv391 <hoangnv391@gmail.com>
2024-05-18 20:48:52 +0700

    approximate complete home page

---
+++ app_test_db/static/scripts/main.js
@@ -0,0 +1,36 @@
+function formatCurrency(value) {
+    // Loại bỏ ký tự đồng Việt Nam (₫) và chuyển đổi chuỗi thành số
+    value = parseInt(value.replace(/₫/g, ''));
+    // Định dạng số tiền và thêm ký tự đồng Việt Nam (₫) vào cuối
+    return value.toLocaleString('vi-VN') + '₫';
+}
+
+// Lấy tất cả các thẻ p có class là "item-price"
+const priceElements = document.querySelectorAll('.item-price');
+
+// Lặp qua từng thẻ p và cập nhật nội dung
+priceElements.forEach(function(element) {
+    element.textContent = formatCurrency(element.textContent);
+});
+
+const imageItems = document.querySelectorAll('.item-option');
+const displayImages = document.querySelectorAll('.item-image img');
+
+imageItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        const imagePath = item.getAttribute('data-src');
+        const correspondingDisplayImage = item.closest('.product-item').querySelector('.item-image img');
+        correspondingDisplayImage.src = imagePath;
+
+        const itemOption = item.getAttribute('data-option');
+        const correspondingDisplayOption = item.closest('.product-item').querySelector('.item-text small');
+        correspondingDisplayOption.innerHTML = itemOption;
+
+        const itemPrice = item.getAttribute('data-price');
+        const correspondingDisplayPrice = item.closest('.product-item').querySelector('p.item-price');
+        correspondingDisplayPrice.innerHTML = formatCurrency(itemPrice);
+    });
+});
+
+
+
